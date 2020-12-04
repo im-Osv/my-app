a4f4a9b8fbff8e3aa429fca468605e9991474de0
Osvaldo Colín <warcraft_wero@msn.com>
2020-12-04 13:31:05 -0600

    Se modificaron los archivos package con al agregar axio para leer datos del backend y WelcomeComponent para hacer llamada a estos

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService.js'
 
 class WelcomeComponent extends Component {
     constructor(props) {
         super(props)
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.state = {
+            welcomeMessage: ''
+        }
+        this.handleSucessfulResponse = this.handleSucessfulResponse.bind(this)
     }
 
     render() {
@@ -12,20 +17,29 @@ class WelcomeComponent extends Component {
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                    Welcome {this.props.match.params.name}. 
+                    Welcome {this.props.match.params.name}.
                     You can manage your todos <Link to="/todos">here</Link>
                 </div>
                 <div className="container">
                     Click here to get a customized welcome message.
-                    <button onClick={this.retrieveWelcomeMessage} 
-                    className='btn btn-success'>Get Welcome Message</button>
+                    <button onClick={this.retrieveWelcomeMessage}
+                        className='btn btn-success'>Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
                 </div>
             </>
         )
     }
 
     retrieveWelcomeMessage() {
-        console.log('retrieve clicked');
+        HelloWorldService.executeHelloWorldService()
+            .then(response => this.handleSucessfulResponse(response))
+        //.catch()
+    }
+
+    handleSucessfulResponse(response) {
+        this.setState({welcomeMessage: response.data})   
     }
 }
 
