b83c5f7e7a517e56433010b704e66fd9f0aeb7d0
Osvaldo Colín <warcraft_wero@msn.com>
2020-12-10 14:46:36 -0600

    Creación y mapeo de los componentes DELETE

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
@@ -0,0 +1,74 @@
+package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
+import java.util.Date;
+
+public class Todo {
+	private long id;
+	private String username;
+	private String description;
+	private Date targetData;
+	private boolean isDone;
+	
+	public Todo(long id, String username, String description, Date targetData, boolean isDone) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.description = description;
+		this.targetData = targetData;
+		this.isDone = isDone;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getTargetData() {
+		return targetData;
+	}
+	public void setTargetData(Date targetData) {
+		this.targetData = targetData;
+	}
+	public boolean isDone() {
+		return isDone;
+	}
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,39 +1,97 @@
 
 import React, { Component } from 'react'
+import TodoDataService from '../../api/todo/TodoDataService.js'
+import AuthenticationService from './AuthenticationService.js'
 
 class ListTodosComponent extends Component {
     constructor(props) {
+        console.log('constructor')
         super(props)
         this.state = {
-            todos:
-                [
-                    { id: 1, description: 'Learn React', done: false, targetDate: new Date() },
-                    { id: 2, description: 'Become an Expert at React', done: false, targetDate: new Date() },
-                    { id: 3, description: 'Visit MX', done: false, targetDate: new Date() }
-                ]
+            todos : [],
+            message: null
         }
+        this.deleteTodoClicked = this.deleteTodoClicked.bind(this)
+        this.refreshTodos = this.refreshTodos.bind(this)
     }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+        this.refreshTodos();
+        console.log(this.state)
+    }
+
+    refreshTodos() {
+        let username = AuthenticationService.getLoggedInUserName()
+        TodoDataService.retrieveAllTodos(username)
+            .then(
+                response=>{
+                    this.setState({todos : response.data})
+                }
+            )
+    }
+
+    deleteTodoClicked(id){
+        let username = AuthenticationService.getLoggedInUserName()
+        console.log(id + " "+ username)
+        TodoDataService.deleteTodo(username,id)
+            .then (
+                response => {
+                    this.setState({message : `Delete of todo ${id} Sucessful`})
+                    this.refreshTodos()
+                }
+            )
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate')
+        console.log(nextProps)
+        console.log(nextState)
+        return true
+    }
+
     render() {
+        console.log('render')
         return (
             <div>
                 <h1>List Todos</h1>
+                {this.state.message && <div className="alert alert-success">{this.state.message}</div>}
                 <div className="container">
                     <table className="table">
                         <thead>
                             <tr>
                                 <th>Description</th>
-                                <th>Is Completed?</th>
                                 <th>Target Date</th>
+                                <th>Is Completed?</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
+                                /*
+                                    Retrieve all Todos for a user
+                                    GET /users/{username}/todos
+
+                                    Delete a Todo of a user
+                                    DELETE /users/{username}/todos/{id}
+
+                                    Edit/Update a Todo
+                                    PUT /users/{username}/todos/{todo_id}
+
+                                    Create a new Todo
+                                    POST /users/{username}/todos/
+                                */
                                 this.state.todos.map(
                                     todo =>
                                         <tr key={todo.id}>
                                             <td>{todo.description}</td>
+                                            <td>{todo.targetData.toString()}</td>
                                             <td>{todo.done.toString()}</td>
-                                            <td>{todo.targetDate.toString()}</td>
+                                            <td><button className="btn btn-warning" onClick={() => this.deleteTodoClicked(todo.id)}>Delete</button></td>
                                         </tr>
                                 )
                             }
